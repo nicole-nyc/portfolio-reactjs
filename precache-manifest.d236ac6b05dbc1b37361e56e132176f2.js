28ce3f4a4c048a37d7650f102ea522ef3c43407d
Nicole <nicole.nayoung@gmail.com>
2018-10-13 22:10:38 -0700

    Updates

---
+++ precache-manifest.d236ac6b05dbc1b37361e56e132176f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f19fe27c3e88be15ec21",
+    "url": "/portfolio-reactjs/static/js/runtime~main.f19fe27c.js"
+  },
+  {
+    "revision": "e5f8b4f3a1e29b59987f",
+    "url": "/portfolio-reactjs/static/js/main.e5f8b4f3.chunk.js"
+  },
+  {
+    "revision": "4b73463d3533eabaafa8",
+    "url": "/portfolio-reactjs/static/js/1.4b73463d.chunk.js"
+  },
+  {
+    "revision": "e5f8b4f3a1e29b59987f",
+    "url": "/portfolio-reactjs/static/css/main.ad7e40ce.chunk.css"
+  },
+  {
+    "revision": "24cab0688b5925761e23af98228180cc",
+    "url": "/portfolio-reactjs/index.html"
+  }
+];
